367c46ae012113e2595b219889b76dc29f163eff
sysadmin-exe <victor.chukwuemeke@wizeline.com>
2021-09-24 04:45:18 +0100

    handling secrets

---
+++ envsetup_copy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#DB DETAILS
+# To use this, kindly fill in the details you want  to use
+export DB_USERNAME=""
+export DB_PASSWORD=""
+export DB_NAME=""
--- mongo-init-copy.js
+++ mongo-init-copy.js
@@ -1,17 +1,17 @@
-db.auth('surveyadmin', 'mongoadmin')
+db.auth('db_username', 'db_passwd')
 
-db = db.getSiblingDB('survey')
+db = db.getSiblingDB('db_name')
 
 db.createCollection('sample_collection');
 
 db.createUser(
     {
-        user: 'surveyadmin',
-        pwd: 'mongoadmin',
+        user: 'db_username',
+        pwd: 'db_passwd',
         roles: [
             {
                 role: 'readWrite',
-                db: 'survey'
+                db: 'db_name'
             }
         ]
     }
