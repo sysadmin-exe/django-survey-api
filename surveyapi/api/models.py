8072d68a52af11e609211ba3b96d7cc3659d7b7a
sysadmin-exe <victor.chukwuemeke@wizeline.com>
2021-09-28 17:49:24 +0100

    removed comments

--- surveyapi/api/models.py
+++ surveyapi/api/models.py
@@ -5,25 +5,6 @@ from django.utils import timezone
 from django.core.exceptions import ValidationError
 
 
-#     #Model for Questions
-# class SurveyQuestion(models.Model):
-#     id = models.AutoField(primary_key=True)
-#     surveyQuestion = models.CharField(max_length=240)
-#     CreatedAt = models.DateTimeField(default=timezone.now)
-
-# class SurveyOption(models.Model):
-#     id = models.AutoField(primary_key=True)
-#     survey_option = models.CharField(max_length=120)
-#     CreatedAt = models.DateTimeField(default=timezone.now)
-#     question = models.ForeignKey(SurveyQuestion, on_delete=models.DO_NOTHING)
-
-# #Model for answers to survey questions
-# class SurveyAnswer(models.Model):
-#     surveyQuestion = models.ForeignKey(SurveyQuestion, on_delete=models.DO_NOTHING)
-#     surveyAnswer = models.ForeignKey(SurveyOption, on_delete=models.DO_NOTHING)
-#     CreatedAt = models.DateTimeField(default=timezone.now)
-
-
 #Model for Questions
 class SurveyQuestion(models.Model):
     surveyQuestion = models.CharField(max_length=240)
