53f0ba48cf5106ec314d3c4524523a478523aa2d
sysadmin-exe <victor.chukwuemeke@wizeline.com>
2021-09-29 10:11:43 +0100

    removed comments

--- surveyapi/api/serializers.py
+++ surveyapi/api/serializers.py
@@ -2,25 +2,6 @@ from django.db.models import fields
 from rest_framework import serializers 
 from api.models import SurveyQuestion, SurveyAnswer, SurveyOption
 
-
-# class SurveyQuestionSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=SurveyQuestion
-#         fields=('surveyQuestion',)
-#         depth = 1
-
-# class SurveyOptionSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=SurveyOption
-#         fields=('question', 'survey_option')
-
-# class SurveyAnswerSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=SurveyAnswer
-#         fields=('surveyQuestion', 'surveyAnswer')
-#         depth = 1
-
-
 class SurveyQuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model=SurveyQuestion
