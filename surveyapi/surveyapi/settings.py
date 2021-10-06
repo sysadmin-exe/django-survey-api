e83ca75c8f9f43f793ffb0982cbabb7b5665ee76
sysadmin-exe <victor.chukwuemeke@wizeline.com>
2021-10-06 21:26:45 +0100

    added secret

--- surveyapi/surveyapi/settings.py
+++ surveyapi/surveyapi/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+#SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-vq6r!*e(009!kmulfz7zuw7d)3m^@bdeoa!nes4*swu=bgyqqu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
